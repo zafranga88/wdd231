757857c66bba606f36d7c5965c336bf32d9f7c4f
zafranga88 <zfrancogabriel@hotmail.com>
2025-07-16 12:57:54 -0300

    yes

---
+++ scripts/modules.mjs
@@ -0,0 +1,73 @@
+const byuiCourse = {
+  code: "WDD231",
+  name: "Web Frontend Development I",
+  sections: [
+    {
+      sectionNumber: 1,
+      enrolled: 88,
+      instructor: "Brother Bingham",
+    },
+    {
+      sectionNumber: 2,
+      enrolled: 81,
+      instructor: "Sister Shultz",
+    },
+    {
+      sectionNumber: 3,
+      enrolled: 95,
+      instructor: "Sister Smith",
+    },
+  ],
+  changeEnrollment: function (sectionNumber, add = true) {
+    // Find the section with the given section number
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNumber == sectionNumber
+    );
+    if (sectionIndex >= 0) {
+      if (add) {
+        this.sections[sectionIndex].enrolled++;
+      } else {
+        this.sections[sectionIndex].enrolled--;
+      }
+      renderSections(this.sections);
+    }
+  },
+};
+
+function setSectionSelection() {
+  const sectionSelect = document.querySelector("#sectionNumber");
+  byuiCourse.sections.forEach((section) => {
+    const option = document.createElement("option");
+    option.value = section.sectionNumber;
+    option.textContent = `${section.sectionNumber}`;
+    sectionSelect.appendChild(option);
+  });
+}
+
+function setTitle(course) {
+  document.querySelector("#courseName").textContent = course.name;
+  document.querySelector("#courseCode").textContent = course.code;
+}
+
+function renderSections(sections) {
+  const html = sections.map(
+    (section) => `<tr>
+    <td>${section.sectionNumber}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.instructor}</td></tr>`
+  );
+  document.querySelector("#sections").innerHTML = html.join("");
+}
+
+document.querySelector("#enrollStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum);
+});
+document.querySelector("#dropStudent").addEventListener("click", function () {
+  const sectionNum = Number(document.querySelector("#sectionNumber").value);
+  byuiCourse.changeEnrollment(sectionNum, false);
+});
+
+setTitle(byuiCourse);
+setSectionSelection(byuiCourse.sections);
+renderSections(byuiCourse.sections);
